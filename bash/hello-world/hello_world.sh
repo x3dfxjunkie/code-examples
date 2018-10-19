682cd65b094f5cbdfea735e91c461f8c2723b9c7
Russell Anderson <russell.anderson2@nike.com>
2018-10-19 09:47:37 -0700

    fixing unbound variable

--- bash/hello-world/hello_world.sh
+++ bash/hello-world/hello_world.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-# This option will make the script exit when there is an error
-set -o errexit
-# This option will make the script exit when it tries to use an unset variable
-set -o nounset
+[[ "$@" =~ '-d' ]] && set -x
+
+set -e
+set -u
 
 main() {
-  # A string variable containing only the FIRST argument passed to the script,
-  # you can use input="$@" to get a string with ALL arguments
-  input=$1
-
   echo 'Hello, World!'
   return 0
 }
 
-# Calls the main function passing all the arguments to it via '$@'
-# The argument is in quotes to prevent whitespace issues
 main "$@"
 
 exit 0
